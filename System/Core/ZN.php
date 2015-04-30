0ab4e4cb1c80d0b5eafbbe607d1886c564ec803f
ozanbote <ozanbote@windowslive.com>
2015-04-30 21:02:13 +0300

    ZN Framework
    ZN Framework

---
+++ System/Core/ZN.php
@@ -0,0 +1,27 @@
+<?php
+/************************************************************/
+/*                      GLOBAL ACCESS                       */
+/************************************************************/
+/*
+
+Author: Ozan UYKUN
+Site: http://www.zntr.net
+Copyright 2012-2015 zntr.net - Tüm hakları saklıdır.
+
+*/
+class ZN
+{
+	/* SUPER GLOBAL VARIABLE *
+	 *
+	 * Super global variables for direct access
+	 * 
+	 */
+	public static $use;
+	
+	/* ZN DESIGN PATTERNS
+	 *
+	 * Static @var zndynamic
+	 *
+	 */
+	public static $zndynamic;
+}
