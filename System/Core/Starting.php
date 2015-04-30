283a22a03acf55c7e00235c7ae4c41891f545785
Ozan Uykun <ozanbote@windowslive.com>
2015-04-30 21:00:50 +0300

    Update Starting.php

--- System/Core/Starting.php
+++ System/Core/Starting.php
@@ -10,9 +10,34 @@ Copyright 2012-2015 zntr.net - Tüm hakları saklıdır.
 
 */
 
+/* USING VARIABLE DIRECT ACCESS
+ *
+ * Static @var zn::use
+ *
+ */
+zn::$use = using();
+/* USING FUNCTIONAL DIRECT ACCESS
+ *
+ * Global @func this()
+ *
+ */
+function this()
+{
+	return zn::$use;
+}
 
+/* STARTING RUN *
+ *
+ * 
+ * System starting
+ */
 Starting::run();
 
+/* CLASS STARTING *
+ *
+ * 
+ * 
+ */
 class Starting
 {
 	public static function run()
@@ -25,10 +50,31 @@ class Starting
 		
 		// OTOMATİK YÜKLEMELER İŞLENİYOR...
 		
-		autoload(config::get('Autoload', 'Library')  , 'Libraries');
-		autoload(config::get('Autoload', 'Tool')   , 'Tools');
-		autoload(config::get('Autoload', 'Language')   , 'Languages');
-		autoload(config::get('Autoload', 'Coder')   , 'Coder');
+		$autoload = config::get('Autoload');
+			
+		autoload($autoload['library']  , 'Libraries');
+		autoload($autoload['tool']     , 'Tools');
+		autoload($autoload['language'] , 'Languages');
+		autoload($autoload['coder']    , 'Coder');
+		
+		// ----------------------------------------------------------------------
+		
+		// COMPOSER AUTOLOAD
+		
+		if($autoload['composer'] === true)
+		{
+			file_exists('vendor/autoload.php')
+				? require_once('vendor/autoload.php')
+				: report('Error','vendor/autoload.php was not found.','AutoloadComposer');
+		}
+		elseif (file_exists($autoload['composer']))
+		{
+			require_once($autoload['composer']);
+		}
+		else
+		{
+			report('Error', $autoload['composer'].' was not found.','AutoloadComposer');
+		}
 		
 		// ----------------------------------------------------------------------
 	
