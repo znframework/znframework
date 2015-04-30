275ed7410ed9797781a6f0fa04c02af93282b883
Ozan Uykun <ozanbote@windowslive.com>
2015-04-30 23:47:09 +0300

    Update Autoload.php

--- Config/Autoload.php
+++ Config/Autoload.php
@@ -24,23 +24,30 @@ SETTINGS
 // System/ dizininde yer alan Database Sınıflarını kullanmak için
 // Sorgulama işlemleri için DbQueryBuilder
 // Oluşturma işlemleri için DbForge sınıf isimlerini kullanınız.
-$config['Autoload']['Library'] 	= array(); // Array
+$config['Autoload']['library'] 	= array(); // Array
 
 /* TOOL	*/
 // İşlev:Otomatik olarak araç yüklemek için kullanılır
 // Parametre:Dahil etmek istediğiniz araçları diziye elaman olarak sırayla ekleyin.
 // Örnek: array("Cleaner", "Rounder");
-$config['Autoload']['Tool'] 	= array(); // Array
+$config['Autoload']['tool'] 	= array(); // Array
 
 /* CODER	*/
 // İşlev:Otomatik olarak kodlayıcı dosyası yüklemek için kullanılır
 // Parametre:Dahil etmek istediğiniz kodlayıcı dosyalarını diziye elaman olarak sırayla ekleyin.
 // Örnek: array("CoderPage1", "CoderPage2");
-$config['Autoload']['Coder'] 	= array(); // Array
+$config['Autoload']['coder'] 	= array(); // Array
 
 /* LANGUAGE	*/
 // İşlev:Otomatik olarak dil dosyası yüklemek için kullanılır
 // Parametre:Dahil etmek istediğiniz dil dosyalarını diziye elaman olarak sırayla ekleyin.
 // Örnek: array("DilDosyasi1", "DilDosyasi2");
-$config['Autoload']['Language'] = array(); // Array
+$config['Autoload']['language'] = array(); // Array
+
+/* COMPOSER AUTOLOAD	*/
+// İşlev:Composer autoload dosyasının yüklenip yüklenilmeyeceğine karar verir.
+// Parametre:True, false veya yol değeri alır. True, vendor/autoload.php dosyasının yüklnemesi anlamına gelir.
+// Parametre olarak yol değeri belirtilebilir. Örnek: example/vendor/autoload.php
+// Örnek: true veya false
+$config['Autoload']['composer'] = false;
 //--------------------------------------------------------------------------------------------------------------------------
