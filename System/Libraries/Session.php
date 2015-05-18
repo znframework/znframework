4f64298a967e2e66bd5eeeab879d623877d80b7e
Ozan Uykun <ozanbote@windowslive.com>
2015-05-18 19:27:01 +0300

    Update Session.php

--- System/Libraries/Session.php
+++ System/Libraries/Session.php
@@ -9,98 +9,221 @@ Site: http://www.zntr.net
 Copyright 2012-2015 zntr.net - Tüm hakları saklıdır.
 
 */
+/******************************************************************************************
+* Config/Session.php dosyasından Ini ayarlarını yapılandır.                               *
+******************************************************************************************/
 config::iniset(config::get('Session','settings'));
-if(!isset($_SESSION)) session_start();
+/******************************************************************************************
+* Herhangi bir oturum başlatılmamışsa oturumu başlat.                                     *
+******************************************************************************************/
+if( ! isset($_SESSION) ) session_start();
 class Sess
 {
 
+	/******************************************************************************************
+	* INSERT                                                                                  *
+	*******************************************************************************************
+	| Genel Kullanım: Oturum oluşturmak için kullanılır.								      |
+	|															                              |
+	| Parametreler: 2 parametresi vardır.                                                     |
+	| 1. string var @name => Oluşturulacak oturumun adı.		    					      |
+	| 2. mixed var @value => Oluşturulacak oturumun tutacağı değer.         			      |
+	|          																				  |
+	| Örnek Kullanım: insert('isim', 'Değer');       										  |
+	| Not: Application/Config/Session.php dosyası üzerinden ayarlarını yapılandırabilirsiniz. |
+	|          																				  |
+	******************************************************************************************/
 	public static function insert($name = '', $values = '')
 	{
-		if(empty($name)) return false;
+		if( empty($name) ) 
+		{
+			return false;
+		}
 		
-		if(is_array($name))
+		$sess_config = config::get('Session');
+		
+		if( is_array($name) )
 		{
 			foreach($name as $key => $value)
 			{
-				if(config::get('Session','encode') === true)
-					$_SESSION[md5($key)] = $value;
+				if( is_hash($sess_config['encode']) )
+				{
+					$_SESSION[hash($sess_config['encode'], $key)] = $value;
+				}
 				else
+				{
 					$_SESSION[$key] = $value;
+				}
 			}
 		}
 		else
 		{
-			if(config::get('Session','encode') === true)
-				$_SESSION[md5($name)] = $values;
+			if( is_hash($sess_config['encode']) )
+			{
+				$_SESSION[hash($sess_config['encode'], $name)] = $values;
+			}
 			else
+			{
 				$_SESSION[$name] = $values;
+			}
 		}
 		
-		session_regenerate_id();
+		if( $sess_config['regenerate'] === true )
+		{
+			session_regenerate_id();
+		}
 	}
 	
+	/******************************************************************************************
+	* SELECT                                                                                  *
+	*******************************************************************************************
+	| Genel Kullanım: Oluşturulan oturumu seçmek için kullanılır.							  |
+	|															                              |
+	| Parametreler: Tek parametresi vardır.                                                   |
+	| 1. string var @name => Seçilecek oturumun ismi.		    					          |
+	|          																				  |
+	| Örnek Kullanım: select('isim');       										          |
+	|          																				  |
+	******************************************************************************************/
 	public static function select($name = '')
 	{
-		if(empty($name)) return false;
+		if( empty($name) ) 
+		{
+			return false;
+		}
 		
-		if(is_array($name))
+		$sess_config = config::get('Session','encode');
+		
+		if( is_array($name) )
 		{
 			foreach($name as $key)
 			{
-				if(config::get('Session','encode') === true)
-					$session[$key] = $_SESSION[md5($key)];
+				if( is_hash($sess_config) )
+				{
+					$session[$key] = $_SESSION[hash($sess_config, $key)];
+				}
 				else
+				{
 					$session[$key] = $_SESSION[$key];
+				}
 			}
 			return $session;
 		}
 		else
 		{
-			if(config::get('Session','encode') === true)
-				if(isset($_SESSION[md5($name)])) 
-					return $_SESSION[md5($name)]; else return false;
+			if( is_hash($sess_config) )
+			{
+				if( isset($_SESSION[hash($sess_config, $name)]) ) 
+				{
+					return $_SESSION[hash($sess_config, $name)]; 
+				}
+				else 
+				{
+					return false;
+				}
+			}
 			else
-				if(isset($_SESSION[$name]))
-					return $_SESSION[$name]; else return false;
+			{
+				if( isset($_SESSION[$name]) )
+				{
+					return $_SESSION[$name]; 
+				}
+				else 
+				{
+					return false;
+				}
+			}
 		}
 		
 	}
 	
+	/******************************************************************************************
+	* SELECT ALL                                                                              *
+	*******************************************************************************************
+	| Genel Kullanım: Oluşturulmuş tüm oturumlara erişmek için kullanılır.				      |
+	|															                              |
+	| Parametreler: Herhangi bir parametresi yoktur.                                          |
+	|          																				  |
+	| Örnek Kullanım: select_all();       										              |
+	|          																				  |
+	******************************************************************************************/
 	public static function select_all()
 	{
 		return $_SESSION;	
 	}
 
-	
+	/******************************************************************************************
+	* DELETE                                                                                  *
+	*******************************************************************************************
+	| Genel Kullanım: Oluşturulan oturumu silmek için kullanılır.							  |
+	|															                              |
+	| Parametreler: Tek parametresi vardır.                                                   |
+	| 1. string var @name => Silinecek oturumun ismi.		    					          |
+	|          																				  |
+	| Örnek Kullanım: delete('isim');       										          |
+	|          																				  |
+	******************************************************************************************/
 	public static function delete($name = '')
 	{
-		if(empty($name)) return false;
+		if( empty($name) ) 
+		{
+			return false;
+		}
 		
-		if(is_array($name))
+		$sess_config = config::get('Session','encode');
+		
+		if( is_array($name) )
 		{
 			foreach($name as $value)
 			{
 				$val = $value;
-				if(config::get('Session','encode') === true)
-					if(isset($_SESSION[md5($val)])) 
-						unset($_SESSION[md5($val)]);
+				
+				if( is_hash($sess_config) )
+				{
+					if( isset($_SESSION[hash($sess_config, $val)]) ) 
+					{
+						unset($_SESSION[hash($sess_config, $val)]);
+					}
+				}
 				else
-					if(isset($_SESSION[$val]))
+				{
+					if( isset($_SESSION[$val]) )
+					{
 						unset($_SESSION[$val]);
+					}
+				}
 			}	
 		}
 		else
 		{
 			$val = $name;
 		}
-		if(config::get('Session','encode') === true)
-			if(isset($_SESSION[md5($val)]))
-				unset($_SESSION[md5($val)]);
+		if( is_hash($sess_config) )
+		{
+			if( isset($_SESSION[hash($sess_config, $val)]) )
+			{
+				unset($_SESSION[hash($sess_config, $val)]);
+			}
+		}
 		else
-			if(isset($_SESSION[$val]))
+		{
+			if( isset($_SESSION[$val]) )
+			{
 				unset($_SESSION[$val]);
+			}
+		}
 	}
 	
+	/******************************************************************************************
+	* DELETE ALL                                                                              *
+	*******************************************************************************************
+	| Genel Kullanım: Oluşturulmuş tüm oturumları silmek için kullanılır.				      |
+	|															                              |
+	| Parametreler: Herhangi bir parametresi yoktur.                                          |
+	|          																				  |
+	| Örnek Kullanım: delete_all();       										              |
+	|          																				  |
+	******************************************************************************************/
 	public static function delete_all()
 	{
 		session_destroy();
