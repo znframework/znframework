de74d763d981361330e23e5c93e741fa4f295a6a
Ozan Uykun <ozanbote@windowslive.com>
2015-05-19 00:17:41 +0300

    Update Xml.php

--- System/Libraries/Xml.php
+++ System/Libraries/Xml.php
@@ -22,10 +22,17 @@ class Xml
 
 	static function load($path = '', $type = 'file')
 	{
-		if( ! is_string($path)) return false;
-		if( ! is_string($type)) $type = 'file';
+		if( ! is_string($path) ) 
+		{
+			return false;
+		}
 		
-		if($type === 'file')
+		if( ! is_string($type) ) 
+		{
+			$type = 'file';
+		}
+		
+		if( $type === 'file' )
 		{
 			self::$xpath = simplexml_load_file(suffix($path, '.xml'));
 			self::$xml_url = $path;
@@ -34,6 +41,7 @@ class Xml
 		{
 			self::$xpath = simplexml_load_string($path);
 		}
+		
 		return self::$xpath;
 	}
 	
@@ -41,9 +49,13 @@ class Xml
 	
 	public static function path($path = '')
 	{
-		if( ! is_string($path)) return false;
+		if( ! is_string($path) ) 
+		{
+			return false;
+		}
 		
 		$path = str_replace("/","//",$path);
+		
 		return self::$xpath->xpath("//".$path);	
 	}
 	
@@ -51,12 +63,21 @@ class Xml
 	
 	public static function create($version = "1.0", $charset = "iso-8859-8", $output = true)
 	{
-		if( ! is_string($version)) $version = "1.0";
-		if( ! is_string($charset)) $charset = "iso-8859-8";
-		if( ! is_bool($output)) $output = true;
+		if( ! is_string($version) || empty($version) ) 
+		{
+			$version = "1.0";
+		}
 		
-		if(empty($version)) $version = "1.0";
-		if(empty($charset)) $charset = "so-8859-8";
+		if( ! is_string($charset) || empty($charset)) 
+		{
+			$charset = "iso-8859-8";
+		}
+		
+		if( ! is_bool($output) ) 
+		{
+			$output = true;
+		}
+		
 		self::$dom = new DOMDocument($version,$charset);
 		
 		self::$dom->formatOutput = $output;
@@ -71,39 +92,66 @@ class Xml
 	
 	public static function add_element($add = '', $to = '')
 	{
-		if( ! is_string($add)) return false;
-		
-		if( ! is_object($to)) $to = '';
-		
-		if(empty($add))
+		if( ! is_string($add) || empty($add) ) 
+		{
 			return false;
+		}
+		
+		if( ! is_object($to) ) 
+		{
+			$to = '';
+		}
 		
 		$add = self::$dom->createElement($add);
-		if(empty($to)) self::$dom->appendChild($add); else $to->appendChild($add); return $add;
+		
+		if( empty($to) ) 
+		{
+			self::$dom->appendChild($add); 
+		}
+		else 
+		{
+			$to->appendChild($add); 
+			
+			return $add;
+		}
 		
 		return $add;
 	}
 	
 	public static function remove_element($add = '', $to = '')
 	{
-		if( ! is_object($add))return false;
-			
-		if(empty($add))
-			return false;
-			
-		if( ! is_array($to))
+		if( ! is_object($add) || empty($add) )
 		{
-			if( ! is_object($to)) return false;
-			if($add->hasChildNodes() <= $to->hasChildNodes()) return false;
+			return false;
+		}
+		
+		if( ! is_array($to) )
+		{
+			if( ! is_object($to) ) 
+			{
+				return false;
+			}
+			
+			if( $add->hasChildNodes() <= $to->hasChildNodes() ) 
+			{
+				return false;
+			}
+			
 			$add->removeChild($to); 
 		}
 		else
+		{
 			foreach($to as $e)
 			{
-				if(is_object($e))
-					if($add->hasChildNodes() > $e->hasChildNodes()) 
+				if( is_object($e) )
+				{
+					if( $add->hasChildNodes() > $e->hasChildNodes() ) 
+					{
 						$add->removeChild($e); 
+					}
+				}
 			}
+		}
 	}
 	
 	// oluşturulan elementlerin için yazı ekleme için kullanılır.
@@ -113,72 +161,114 @@ class Xml
 	
 	public static function add_content($to = "", $text = "")
 	{
-		if( ! is_object($to))return false;
-		if( ! (is_string($text) || is_numeric($text))) $text = "";
+		if( ! is_object($to) )
+		{
+			return false;
+		}
+		
+		if( ! is_char($text) ) 
+		{
+			$text = '';
+		}
 		
 		$text = self::$dom->createTextNode($text);
-		if(empty($to)) $to = self::$dom;
+		
+		if( empty($to) ) 
+		{
+			$to = self::$dom;
+		}
 		
 		$to->appendChild($text);
 	}
 	
 	public static function add_attr($element = '', $name = '', $value = '')
 	{
-		if( ! is_object($element)) return false;
-		if(empty($element))
-			return false;
-			
-		if( ! is_array($name))
+		if( ! is_object($element) || empty($element) ) 
 		{
-			if( ! is_string($value)) $value = '';
+			return false;
+		}
+		
+		if( ! is_array($name) )
+		{
+			if( ! is_string($value) ) 
+			{
+				$value = '';
+			}
+			
 			$element->setAttribute($name , $value);
 		}
 		else
+		{
 			foreach($name as $n => $v)
+			{
 				$element->setAttribute($n , $v);
+			}
+		}
 	}
 	
 	public static function remove_attr($element = '', $name = '')
 	{
-		if( ! is_object($element)) return false;
-		if( ! (is_string($name) || is_numeric($name)))$name = "";
-		if(empty($element))
+		if( ! is_object($element) || empty($element) ) 
+		{
 			return false;
+		}
+		
+		if( ! is_char($name) )
+		{
+			$name = '';
+		}
 			
-		if( ! is_array($name))
+		if( ! is_array($name) )
+		{
 			$element->removeAttribute($name);
+		}
 		else
+		{
 			foreach($name as $n)
+			{
 				$element->removeAttribute($n);
+			}
+		}
 	}
 	
 	public static function get_attr($element = '', $name = '')
 	{
-		if( ! is_object($element)) return false;
-		if(empty($element))
-			return false;
-			
-		if( ! is_array($name))
+		if( ! is_object($element) || empty($element) ) 
 		{
-			if( ! is_string($value)) $value = '';
+			return false;
+		}
+			
+		if( ! is_array($name) )
+		{
+			if( ! is_string($value) ) 
+			{
+				$value = '';
+			}
+			
 			return $element->getAttribute($name);
 		}
 		else
 		{
 			$attr = array();
+			
 			foreach($name as $n)
+			{
 				$attr[$n] = $element->getAttribute($n);
+			}
+			
 			return $attr;
 		}
 	}
 	
 	public static function get_contents_by_name($name = '')
 	{
-		if( ! is_string($name)) return false;
-		if(empty($name))
+		if( ! is_string($name) || empty($name) ) 
+		{
 			return false;
+		}
 		
-		$all = "";
+		$all = '';
+		
 		$elements = self::$dom->getElementsByTagName($name);
 		
 		foreach($elements as $element)
@@ -191,20 +281,22 @@ class Xml
 	
 	public static function get_content_by_id($id = '')
 	{
-		if( ! is_string($id)) return false;
-		
-		if(empty($id))
+		if( ! is_string($id) || empty($id) ) 
+		{
 			return false;
+		}
 			
 		$element = self::$dom->getElementById($id);
+		
 		return $element->textContent;
 	}
 	
 	public static function get_content($name = '')
 	{
-		if( ! is_object($name)) return false;
-		if(empty($name))
+		if( ! is_object($name) || empty($name) ) 
+		{
 			return false;
+		}
 			
 		return $name->textContent;
 	}
@@ -217,10 +309,10 @@ class Xml
 	{
 		$dom = self::$dom;
 		
-		if(isset(self::$dom)) self::$dom = NULL;
-		if(isset(self::$xml_url)) self::$xml_url = NULL;
-		if(isset(self::$xpath)) self::$xpath = NULL;
-		if(isset(self::$simple_xml)) self::$simple_xml = NULL;
+		if( isset(self::$dom) ) 		self::$dom = NULL;
+		if( isset(self::$xml_url) )	 	self::$xml_url = NULL;
+		if( isset(self::$xpath) ) 		self::$xpath = NULL;
+		if( isset(self::$simple_xml) ) 	self::$simple_xml = NULL;
 		
 		return $dom->saveXML();
 	}
