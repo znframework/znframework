1e7c8dad4a05d1fa212e41a8146b60315c390f01
Ozan Uykun <ozanbote@windowslive.com>
2015-05-19 18:31:36 +0300

    Update Captcha.php

--- System/Tools/Captcha.php
+++ System/Tools/Captcha.php
@@ -10,41 +10,69 @@ Copyright 2012-2015 zntr.net - Tüm hakları saklıdır.
 
 */
 
-// Function: create_captcha()
-// İşlev: Güvenlik kodu oluşturmak için kullanılır.
-// Parametreler
-// @img = true olması durumunda güvenlik kodunu img etiketi içerisinde verir.
-// false olması durumunda ise captcha dosyasının img içinde kullanılmak üzere yolunu verir.
-// Dönen Değer: Güvenlik kodu yolu.
-
+/******************************************************************************************
+* CREATE CAPTCHA                                                                          *
+*******************************************************************************************
+| Genel Kullanım: Güvenlik kodu oluşturmak için kullanılır. 							  |
+|																						  |
+| Parametreler: Tek parametresi vardır.                                              	  |
+| 1. boolean var @img => <img> nesnesi oluşturulsun mu?.						     	  |
+|          																				  |
+| Örnek Kullanım:         																  |
+| echo create_captcha(true);															  |
+|																						  |
+| $kod = create_captcha(); 																  |
+| echo '<img src="'.$kod.'" />'; 														  |
+|																						  |
+******************************************************************************************/	
 if( ! function_exists('create_captcha') )
 {
 	function create_captcha($img = false)
 	{
-		if( ! is_bool($img)) $img = false;
+		if( ! is_bool($img) ) 
+		{
+			$img = false;
+		}
 		
-		if( ! isset($_SESSION)) session_start();
+		if( ! isset($_SESSION) ) 
+		{
+			session_start();
+		}
 		
 		$set = config::get("Captcha", "settings");	
+		
 		$_SESSION[md5('captcha_code')] = substr(md5(rand(0,999999999999999)),-($set["char_count"]));	
 		
-		if($img)	
+		if( $img === true )
+		{	
 			return '<img src="'.base_url(CORE_DIR."Captcha.php").'">';
+		}
 		else
+		{
 			return base_url(CORE_DIR."Captcha.php");
+		}
 	}	
 }
 
-// Function: get_captcha_code()
-// İşlev: Daha önce oluşturulan güvenlik uygulamasının kod bilsini verir.
-// Parametreler: Yok.
-// Dönen Değer: Güvenlik kodu bilgisi.
-
+/******************************************************************************************
+* GET CAPTCHA CODE                                                                        *
+*******************************************************************************************
+| Genel Kullanım: Daha önce oluşturulan güvenlik uygulamasının kod bilgini verir. 		  |
+|																						  |
+| Parametreler: Herhangi bir parametresi yoktur.                                          |
+|          																				  |
+| Örnek Kullanım:         																  |
+| echo get_captcha_code(); // Çıktı: 1A4D31 											  |
+|																						  |
+******************************************************************************************/	
 if( ! function_exists('get_captcha_code') )
 {
 	function get_captcha_code()
 	{
-		if(!isset($_SESSION)) session_start();
+		if( ! isset($_SESSION) ) 
+		{
+			session_start();
+		}
 		
 		return $_SESSION[md5('captcha_code')];
 	}	
