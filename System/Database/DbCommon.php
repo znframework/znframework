0a1914baa66147c44e2f9f90a1055202c1a069b2
Ozan Uykun <ozanbote@windowslive.com>
2015-05-19 20:20:47 +0300

    Update DbCommon.php

--- System/Database/DbCommon.php
+++ System/Database/DbCommon.php
@@ -9,15 +9,30 @@ Site: http://www.zntr.net
 Copyright 2012-2015 zntr.net - Tüm hakları saklıdır.
 
 */
+
+/******************************************************************************************
+* DB COMMON                                                                               *
+*******************************************************************************************
+| Genel Kullanım: Veritabanı sürüleri için ortak bir kullanım oluşturulmuştur.    		  |
+| başında kullanılır.										  							  |
+|															                              |
+| Parametreler: Herhangi bir parametresi yoktur.                                          |
+|          																				  |
+******************************************************************************************/
 function dbcommon()
 {	
 	$config = config::get('Database');
 	
-	if(isset($config['driver'])) 
-	{			
+	if( isset($config['driver']) ) 
+	{	
+		// Drivere ayarına girilen verinin
+		// ilk harfini büyük yapması isteniyor.
+		// pdo => Pdo		
 		$driver = ucwords($config['driver']);
 		
-		if(strpos($config['driver'], '->'))
+		// Sub driver kullanılırken driver->subdriver
+		// kullanımı için böyle bir kontrol yapılmaktadır.
+		if( strpos($config['driver'], '->') )
 		{
 			$subdrivers = explode('->', $config['driver']);
 			$driver  = $subdrivers[0];
@@ -25,14 +40,20 @@ function dbcommon()
 		
 		$driver_path = DRIVERS_DIR.$driver.'.php';
 		
-		if( is_file($driver_path) )			
+		// Hangi sürücü kullanılacaksa
+		// o sürüyü dahil ediyor.
+		if( is_file($driver_path) )	
+		{		
 			require_once($driver_path);
+		}
 		else
 		{
 			die(get_message('Database', 'db_driver_error', $driver));
 		}
+		
 		$driver = $driver.'Driver';
 		
+		// Sürüden bir nesne oluşturuluyor.
 		$db = new $driver;
 		
 		return $db;
