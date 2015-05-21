39c53835380f4afcc7712b50fa14227674924973
Ozan Uykun <ozanbote@windowslive.com>
2015-05-21 20:36:15 +0300

    Update Config.php

--- System/Core/Config.php
+++ System/Core/Config.php
@@ -36,13 +36,18 @@ class Config
 	 */
 	private static $load_control = false;
 	
+	// config() yöntemi için oluşturulmuştur.
+	// Parametreye göre ayar dosyasını yükler.
 	private static function _config($file)
 	{
 		global $config;
 		
 		$path = CONFIG_DIR.suffix($file,".php");
 		
-		if( ! is_file($path) ) return false;
+		if( ! is_file($path) ) 
+		{
+			return false;
+		}
 		
 		if( ! is_import($path) ) 
 		{
@@ -63,22 +68,37 @@ class Config
 	 */
 	public static function get($file = '', $configs = '')
 	{	
-		if( ! is_string($file)) return false;
-		
-		if(empty($file)) return false;	
+		if( ! is_string($file) || empty($file) ) 
+		{
+			return false;
+		}
 		
 		self::_config($file);
 		
-		if(empty($configs))  
+		if( empty($configs) )  
 		{
 			if( isset( self::$set_configs[$file] ) )
 			{
 				self::$config[$file][key(self::$set_configs[$file])] = current(self::$set_configs[$file]);
 			}
 			
-			if( isset(self::$config[$file]) ) return self::$config[$file]; else return false;
+			if( isset(self::$config[$file]) ) 
+			{
+				return self::$config[$file]; 
+			}
+			else 
+			{
+				return false;
+			}
 		}
-		if( isset(self::$config[$file][$configs]) ) return self::$config[$file][$configs]; else return false;
+		if( isset(self::$config[$file][$configs]) ) 
+		{
+			return self::$config[$file][$configs]; 
+		}
+		else 
+		{
+			return false;
+		}
 	}
 	
 	/* SET FUNCTION
@@ -93,15 +113,24 @@ class Config
 	 */
 	public static function set($file = '', $configs = '', $set = '')
 	{
-		if( ! is_string($file)) return false;
+		if( ! is_string($file) || empty($file) ) 
+		{
+			return false;
+		}
 		
-		if(empty($file) || empty($configs)) return false;
-	
+		if( empty($configs) ) 
+		{
+			return false;
+		}
+		
 		self::_config($file);
 		
 		self::$set_configs[$file][$configs] = $set;
 		
-		if(isset(self::$config[$file][$configs])) return self::$config[$file][$configs] = $set;	
+		if( isset(self::$config[$file][$configs]) ) 
+		{
+			return self::$config[$file][$configs] = $set;	
+		}
 	}
 	
 	/* INISET FUNCTION
@@ -120,16 +149,19 @@ class Config
 	public static function iniset($key = '', $val = '')
 	{
 		
-		if(empty($key)) return false;
+		if( empty($key) ) 
+		{
+			return false;
+		}
 		
-		if( ! is_array($key))
+		if( ! is_array($key) )
 		{	
-			if(is_array($val))
+			if( is_array($val) )
 			{
 				return false;
 			}
 			
-			if($val !== '') 
+			if( $val !== '' ) 
 			{
 				ini_set($key, $val);
 			}
@@ -138,7 +170,7 @@ class Config
 		{
 			foreach($key as $k => $v)
 			{
-				if($v !== '') 
+				if( $v !== '' ) 
 				{
 					ini_set($k, $v); 			
 				}
@@ -160,19 +192,24 @@ class Config
 	 */
 	public static function iniget($key = '')
 	{
-		if(empty($key)) return false;
+		if( empty($key) ) 
+		{
+			return false;
+		}
 		
-		if( ! is_array($key))
+		if( ! is_array($key) )
 		{	
 			return ini_get($key);
 		}
 		else
 		{
 			$keys = array();
+			
 			foreach($key as $k)
 			{
 				$keys[$k] = ini_get($k);	
 			}
+			
 			return $keys;
 		}
 	}
@@ -183,10 +220,14 @@ class Config
 	 */
 	public static function iniget_all($extension = '', $details = true)
 	{
-		if(empty($extension)) 
+		if( empty($extension) ) 
+		{
 			return ini_get_all();	
+		}
 		else
+		{
 			return ini_get_all($extension, $details);	
+		}
 	}
 	
 	/* INI RESTORE FUNCTION
