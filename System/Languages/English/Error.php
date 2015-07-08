cff02130104082a468a0f37b0746e732fa261eed
Ozan Uykun <ozanbote@windowslive.com>
2015-07-08 22:45:04 +0300

    ZN Framework 2 July Update V018
    System/Languages/Error.php dosyaları güncellenmiştir.

--- System/Languages/English/Error.php
+++ System/Languages/English/Error.php
@@ -10,6 +10,6 @@ Copyright 2012-2015 zntr.net - Tüm hakları saklıdır.
 
 */
 $lang['Error']['classError'] 				= 'Error: `%` class was not found!';
-$lang['Error']['callUserFuncArrayError'] 	= 'Error: URL does not contain a valid function or method information! & method could not be found!';
-$lang['Error']['notIsFileError'] 		  	= 'Error: URL does not contain a valid path! % file could not be found!';
+$lang['Error']['callUserFuncArrayError'] 	= 'Error: URL does not contain a valid function or method information! `%` method could not be found!';
+$lang['Error']['notIsFileError'] 		  	= 'Error: URL does not contain a valid path! `%` file could not be found!';
 $lang['Error']['fileNotFound']				= 'Error: `%` file was not found!';
