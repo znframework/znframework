f042c6760be3487f65cb100edb7436288e58c12c
ozanbote <ozanbote@windowslive.com>
2016-08-19 14:10:08 +0300

    ZN 4 EIP Version Updated Backend
    ZN 4 EIP Version Updated Backend

---
+++ Projects/Backend/Views/welcome.wizard.php
@@ -0,0 +1,17 @@
+#!DOCTYPE[html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"]:
+
+#html[xmlns="http://www.w3.org/1999/xhtml"]:
+
+    #head
+        #meta[http-equiv="Content-Type" content="text/html; charset=utf-8"]:
+        #title(ZERONEED PHP WEB FRAMEWORK):
+        
+        @Import::font('font'):
+        @Import::style('style'):
+    ##head
+
+    #body
+    	#div[id="logo-image"]: ##div
+    ##body
+
+##html
