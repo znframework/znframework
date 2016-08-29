d4a19e10dbe70a25dbf8c7be22896188619a70ab
ozanbote <ozanbote@windowslive.com>
2016-08-29 11:45:58 +0300

    Created FileSystemCommon Library
    Created FileSystemCommon Library

---
+++ Internal/Libraries/FileSystem/FileSystemCommon.php
@@ -0,0 +1,109 @@
+<?php namespace ZN\FileSystem;
+
+class FileSystemCommon extends \CallController implements FileSystemCommonInterface
+{
+    //--------------------------------------------------------------------------------------------------------
+    //
+    // Author     : Ozan UYKUN <ozanbote@gmail.com>
+    // Site       : www.znframework.com
+    // License    : The MIT License
+    // Telif Hakkı: Copyright (c) 2012-2016, znframework.com
+    //
+    //--------------------------------------------------------------------------------------------------------
+    
+    //--------------------------------------------------------------------------------------------------------
+    // Access
+    //--------------------------------------------------------------------------------------------------------
+    //
+    // @var array
+    //
+    //--------------------------------------------------------------------------------------------------------
+    protected $access = NULL;
+
+    //--------------------------------------------------------------------------------------------------------
+    // Access
+    //--------------------------------------------------------------------------------------------------------
+    //
+    // @param bool $realPath = true
+    // @param bool $parentDirectoryAccess = false
+    //
+    // @param FileSystemCommon
+    //
+    //--------------------------------------------------------------------------------------------------------
+    public function access($realPath = true, $parentDirectoryAccess = false) : FileSystemCommon
+    {
+        $this->access['realPath']              = $realPath;
+        $this->access['parentDirectoryAccess'] = $parentDirectoryAccess;
+
+        return $this;
+    }
+
+    //--------------------------------------------------------------------------------------------------------
+    // Rpath
+    //--------------------------------------------------------------------------------------------------------
+    //
+    // @param string $file
+    //
+    // @param string
+    //
+    //--------------------------------------------------------------------------------------------------------
+    public function rpath(String $file) : String
+    {
+        $config = config('FileSystem', 'file', $this->access);
+
+        $this->access = NULL;
+
+        if( $config['parentDirectoryAccess'] === false )
+        {
+            $file = str_replace('../', '', $file);
+        }
+
+        if( $config['realPath'] === true )
+        {
+            $file = prefix($file, REAL_BASE_DIR);
+
+        }  
+
+        return $file;
+    }
+
+    //--------------------------------------------------------------------------------------------------------
+    // Is Available
+    //--------------------------------------------------------------------------------------------------------
+    //
+    // @param string $file
+    //
+    // @param bool
+    //
+    //--------------------------------------------------------------------------------------------------------
+    public function isAvailable(String $file) : Bool
+    {
+        $file = $this->rpath($file);
+
+        if( file_exists($file) )
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    //--------------------------------------------------------------------------------------------------------
+    // permission()
+    //--------------------------------------------------------------------------------------------------------
+    //
+    // Bir dizin veya dosyaya yetki vermek için kullanılır.
+    //
+    //--------------------------------------------------------------------------------------------------------
+    public function permission(String $name, Int $permission = 0755) : Bool
+    {
+        $name = \File::rpath($name);
+        
+        if( ! file_exists($name) )
+        {
+            return \Exceptions::throws('FileSystem', 'file:notFoundError', $name);
+        }
+
+        return \File::permission($name, $permission);
+    }
+}
