ef0cb581587701572db90b8183f28540171845e3
ozanbote <ozanbote@windowslive.com>
2016-08-29 19:03:35 +0300

    Updated Download Library
    Updated Download Library

--- Internal/Libraries/FileSystem/Download/Download.php
+++ Internal/Libraries/FileSystem/Download/Download.php
@@ -1,6 +1,6 @@
 <?php namespace ZN\FileSystem;
 
-use Exceptions, CallController;
+use Exceptions, CallController, File;
 
 class InternalDownload extends CallController implements DownloadInterface
 {
@@ -22,13 +22,13 @@ class InternalDownload extends CallController implements DownloadInterface
     //--------------------------------------------------------------------------------------------------------
     public function start(String $file)
     {
-        if( ! file_exists($file) )
+        if( ! File::available($file) )
         {
             return Exceptions::throws('FileSystem', 'file:notFoundError', $file);
         }
     
         $fileEx   = explode("/", $file);
-        $fileName = $fileEx[count($fileEx)-1];
+        $fileName = $fileEx[count($fileEx) - 1];
         $filePath = trim($file, $fileName);
         
         header("Content-type: application/x-download");
