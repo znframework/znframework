cf49ee5a7067d88fd6e477e7c02ded5d02cba0c3
ozanbote <ozanbote@windowslive.com>
2016-09-30 16:12:21 +0300

    Updated Json & Cookie Libraries
    Updated Json & Cookie Libraries

--- Internal/Libraries/DataTypes/Json/ErrorInfo.php
+++ Internal/Libraries/DataTypes/Json/ErrorInfo.php
@@ -1,5 +1,7 @@
 <?php namespace ZN\DataTypes\Json;
 
+use Json;
+
 class ErrorInfo implements ErrorInfoInterface
 {
     //--------------------------------------------------------------------------------------------------------
@@ -33,5 +35,19 @@ class ErrorInfo implements ErrorInfoInterface
     public static function no() : Int
     {
         return json_last_error();
+    }
+
+    //--------------------------------------------------------------------------------------------------------
+    // Check
+    //--------------------------------------------------------------------------------------------------------
+    //
+    // @param string $data
+    //
+    //--------------------------------------------------------------------------------------------------------
+    public static function check(String $data) : Bool
+    {
+        return ( is_array(json_decode($data, true)) && self::no() === 0 )
+               ? true
+               : false;
     }
 }
