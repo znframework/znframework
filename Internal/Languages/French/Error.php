3cb5a288000d380f6924ba9acf3c8cf97daad4f7
Ozan Uykun <ozanbote@windowslive.com>
2017-06-22 23:42:03 +0300

    5.0.0 -> Added French Language
    5.0.0 -> Added French Language

---
+++ Internal/Languages/French/Error.php
@@ -0,0 +1,40 @@
+<?php return
+[
+    //----------------------------------------------------------------------------------------------------
+    // Error
+    //----------------------------------------------------------------------------------------------------
+    //
+    // Author     : Ozan UYKUN <ozanbote@windowslive.com> | <ozanbote@gmail.com>
+    // Site       : www.znframework.com
+    // License    : The MIT License
+    // Copyright  : Copyright (c) 2012-2016, ZN Framework
+    //
+    //----------------------------------------------------------------------------------------------------
+
+    'classError'                => 'Erreur: la classe `%` n\'a pas été trouvée!',
+    'controllerNameError'       => 'Erreur: Un contrôleur ne peut pas être identifié par le fichier `%` name!',
+    'notFoundController'        => 'Erreur: L\'URL ne contient pas d\'informations de contrôleur valides! Le contrôleur `%` n\'a pas pu être trouvé!',
+    'callUserFuncArrayError'    => 'Erreur: L\'URL ne contient pas d\'informations valides de la fonction ou de la méthode! La méthode `%` n\'a pas pu être trouvée!',
+    'notIsFileError'            => 'Erreur: L\'URL ne contient pas de chemin d\'accès valide. Le fichier `%` n\'a pas pu être trouvé!',
+    'fileNotFound'              => 'Erreur: le fichier `%` n\'a pas été trouvé!',
+    'fileNotWrite'              => 'Erreur: le fichier `%` ne peut pas créer! Vérifiez les autorisations de création de fichier!',
+    'undefinedFunction'         => 'Erreur: Appel à la fonction non définie `%`!',
+    'undefinedFunctionExtension'=> 'Erreur: l\'extension `%` n\'est pas chargée! Installez pour utiliser les fonctions `%`.',
+    'invalidVersion'            => 'Erreur: Pour utiliser `%` les méthodes doivent être installées, la version PHP `#`!',
+    'driverError'               => 'Le pilote `%` n\'a pas été trouvé!',
+    'hashParameter'             => 'Le paramètre `%` doit contenir le type de données hash algos (md5, sha1)!',
+    'emailParameter'            => 'Le paramètre `%` doit contenir le type de données par courrier électronique!',
+    'objectParameter'           => 'Le paramètre `%` doit contenir le type de données objet!',
+    'resourceParameter'         => 'Le paramètre `%` doit contenir le type de données de ressource!',
+    'callableParameter'         => 'Le paramètre `%` doit contenir le type de données appelable!',
+    'fileParameter'             => 'Le paramètre `%` doit contenir le type de données du fichier!',
+    'emptyParameter'            => 'Le paramètre `%` doit contenir une valeur!',
+    'emptyVariable'             => 'La variable `%` doit contenir une valeur!',
+    'charsetParameter'          => 'Le paramètre `%` doit contenir un jeu de caractères valide!',
+    'invalidInput'              => 'Les informations de saisie "%" ne sont pas valides!',
+    'typeHint'                  => 'Erreur de paramètre invalide! & Paramètre doit être%!',
+    'templateWizard'            => 'Erreur de syntaxe! Vérifiez les symboles:, # et @.
+                                    L\'utilisation de ces symboles peut être oubliée.
+                                    Ces symboles nécessitent / préfixe en utilisation normale.',
+    'invalidRequest'            => 'Erreur: [Demande non valide!] L\'accès par URL de la page est désactivé.'
+];
---
+++ Internal/Languages/French/IndividualStructures.php
@@ -0,0 +1,79 @@
+<?php
+return
+[
+	//----------------------------------------------------------------------------------------------------
+	// Individual Structrues
+	//----------------------------------------------------------------------------------------------------
+	//
+	// Author     : Ozan UYKUN <ozanbote@windowslive.com> | <ozanbote@gmail.com>
+	// Site       : www.znframework.com
+	// License    : The MIT License
+	// Copyright  : Copyright (c) 2012-2016, ZN Framework
+	//
+	//----------------------------------------------------------------------------------------------------
+
+	//----------------------------------------------------------------------------------------------------
+	// Benchmark
+	//----------------------------------------------------------------------------------------------------
+	//
+	// Benchmark Lang Words
+	//
+	//----------------------------------------------------------------------------------------------------
+	'benchmark:elapsedTime'         => 'Temps de chargement du système',
+	'benchmark:memoryUsage'         => 'Utilisation de la mémoire',
+	'benchmark:maxMemoryUsage'      => 'Utilisation maximale de la mémoire',
+	'benchmark:resultTable'         => 'TABLEAU DE RÉSULTATS DE BENCHMARK',
+	'benchmark:performanceTips'     => 'CONSEILS D\'AMÉLIORATION DE LA PERFORMANCE',
+	'benchmark:laterProcess'        => 'Les paramètres suivants sont recommandés après la fin de votre projet.',
+	'benchmark:configAutoloader'    => 'Config/Autoloader.php',
+	'benchmark:configHtaccess'      => 'Config/Htaccess.php',
+	'benchmark:second'              => 'Secondes',
+	'benchmark:byte'                => 'Bytes',
+	'benchmark:countFile'           => 'Count Load Files',
+
+	//----------------------------------------------------------------------------------------------------
+	// Cache
+	//----------------------------------------------------------------------------------------------------
+	//
+	// Cache Lang Words
+	//
+	//----------------------------------------------------------------------------------------------------
+	'cache:driverError'           => 'Le pilote `%` n\'a pas été trouvé!',
+	'cache:unsupported'           => 'L\'extension PHP `%` doit être chargée pour utiliser Wincache Cache!',
+	'cache:invalidDriver'         => 'Le pilote `%` est invalide!',
+	'cache:connectionRefused'     => 'Connexion rejetée! Erreur: `%`',
+	'cache:authenticationFailed'  => 'Authentification échouée!',
+
+	//-----------------------------------------------------------------------------------------------------
+	// User
+	//-----------------------------------------------------------------------------------------------------
+	//
+	// User Lang Words
+	//
+	//-----------------------------------------------------------------------------------------------------
+	'user:registerSuccess'          => 'Votre inscription a été effectuée avec succès.',
+	'user:registerError'            => 'Vous vous êtes déjà inscrit au système pour que la transaction ne puisse pas être effectuée!',
+	'user:registerEmailError'       => 'Votre processus parce que le système n\'a pas pu effectuer un e-mail enregistré précédemment!',
+	'user:registerUsernameError'    => "Les données doivent inclure le nom d'utilisateur et le mot de passe!",
+	'user:loginError'               => 'Échec de la connexion. Le pseudo ou mot de passe est incorect!',
+	'user:bannedError'              => 'Vous ne pouvez pas vous connecter car vous avez été interdit du système!',
+	'user:loginSuccess'             => 'ous avez ouvert une session avec succès. Redéfinir ... Veuillez patienter.',
+	'user:registerUnknownError'     => 'Erreur inconnue!',
+	'user:oldPasswordError'         => 'Vous avez entré le mauvais mot de passe!',
+	'user:passwordNotMatchError'    => 'Les mots de passe ne correspondent pas!',
+	'user:updateProcessSuccess'     => 'Le processus de mise à jour est réussi.',
+	'user:forgotPasswordError'      => "Vous n'êtes pas enregistré dans le système!",
+	'user:forgotPasswordSuccess'    => "Votre mot de passe a été envoyé à votre courrier électronique.",
+	'user:newYourPassword'          => "Envoyé un nouveau mot de passe.",
+	'user:emailError'               => "N'envoyez pas votre mail!",
+	'user:emailImformationError'    => "Des informations par e-mail sont trouvées!",
+	'user:username'                 => "Nom d'utilisateur",
+	'user:password'                 => "Mot de passe",
+	'user:learnNewPassword'         => "Cliquez pour vous connecter avec votre nouveau mot de passe.",
+	'user:activation'               => "Cliquez pour compléter le processus d'activation.",
+	'user:activationProcess'        => "Processus d'activation de l'utilisateur.",
+	'user:activationError'          => "Vous ne pouvez pas vous connecter pour compléter le processus d'activation.",
+	'user:activationEmail'          => "Pour terminer votre inscription, cliquez sur le lien d'activation envoyé à votre adresse e-mail.",
+	'user:activationCompleteError'  => "Le processus d'activation n'a pas pu être complété!",
+	'user:activationComplete'       => "Le processus d'activation est complété avec succès."
+];
