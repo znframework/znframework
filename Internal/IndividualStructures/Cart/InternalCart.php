07d5048c7e3866d5f6325baed08fc577cb49a554
Ozan UYKUN <znframework@gmail.com>
2017-09-24 22:39:07 +0300

    5.3.7 -> Updated
    5.3.7 -> Updated

--- Internal/IndividualStructures/Cart/InternalCart.php
+++ Internal/IndividualStructures/Cart/InternalCart.php
@@ -16,14 +16,21 @@ class InternalCart extends \FactoryController
         'methods' =>
         [
             'insertitem'  => 'Cart\Insert::item',
+            'insert'      => 'Cart\Insert::item',
             'selectitems' => 'Cart\Select::items',
+            'selectall'   => 'Cart\Select::items',
             'selectitem'  => 'Cart\Select::item',
+            'select'      => 'Cart\Select::item',
             'totalitems'  => 'Cart\Total::items',
             'totalprices' => 'Cart\Total::prices',
             'updateitem'  => 'Cart\Update::item',
+            'update'      => 'Cart\Update::item',
             'deleteitem'  => 'Cart\Delete::item',
+            'delete'      => 'Cart\Delete::item',
             'deleteitems' => 'Cart\Delete::items',
+            'deleteall'   => 'Cart\Delete::items',
             'moneyformat' => 'Cart\Money::format',
+            'formatmoney' => 'Cart\Money::format'
         ]
     ];
 }
