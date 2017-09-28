458625bcfdeb219ada31d3832076eb787d64b92c
Ozan UYKUN <znframework@gmail.com>
2017-09-28 23:29:03 +0300

    5.3.7 -> Added Writable Control
    5.3.7 -> Added Writable Control

--- zerocore.php
+++ zerocore.php
@@ -1219,6 +1219,18 @@ function internalProjectContainerDir($path = NULL) : String
 //--------------------------------------------------------------------------------------------------
 function internalCurrentProject()
 {
+    if( ! is_writable('/') )
+    {
+        trace
+        (
+            'Please check the [file permissions]. Click the 
+                <a target="_blank" style="text-decoration:none" href="https://docs.znframework.com/getting-started/installation-instructions#sh42">
+                    [documentation]
+                </a> 
+            to see how to configure file permissions.'
+        );
+    }
+
     if( PROJECT_TYPE === 'SE' )
     {
         define('CURRENT_PROJECT', NULL);
