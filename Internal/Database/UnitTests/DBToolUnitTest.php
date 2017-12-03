5d0a492aaac5222ba3cd3bb0dc4d8615d686f65c
Ozan UYKUN <znframework@gmail.com>
2017-12-03 11:16:26 +0300

    Updated
    Updated

---
+++ Internal/Database/UnitTests/DBToolUnitTest.php
@@ -0,0 +1,27 @@
+<?php namespace ZN\Database;
+
+class DBToolUnitTest extends \UnitTestController
+{
+    //--------------------------------------------------------------------------------------------------------
+    //
+    // Author     : Ozan UYKUN <ozanbote@gmail.com>
+    // Site       : www.znframework.com
+    // License    : The MIT License
+    // Copyright  : (c) 2012-2016, znframework.com
+    //
+    //--------------------------------------------------------------------------------------------------------
+
+    const unit =
+    [
+        'class'   => 'DBTool',
+        'methods' =>
+        [
+            'listDatabases'    => [],
+            'listTables'       => [],
+            'statusTables'     => ['*'],
+            'optimizeTables'   => ['*'],
+            'repairTables'     => ['*'],
+            'backup'           => []
+        ]
+    ];
+}
