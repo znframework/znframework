03eaa0592892a698406752275b33a1988cc4fd81
Ozan UYKUN <znframework@gmail.com>
2018-01-21 23:42:40 +0300

    Updated Filesystem Config

--- Projects/Frontend/Config/Filesystem.php
+++ Projects/Frontend/Config/Filesystem.php
@@ -17,24 +17,5 @@
     [
         'realPath'              => true,
         'parentDirectoryAccess' => false
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | FTP
-    |--------------------------------------------------------------------------
-    |
-    | Includes FTP connection settings.
-    |
-    */
-
-    'ftp' =>
-    [
-        'host'       => '',  
-        'user'       => '',   
-        'password'   => '',   
-        'timeout'    => 90, 
-        'port'       => 21, 
-        'sslConnect' => false 
     ]
 ];
