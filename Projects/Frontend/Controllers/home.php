d9c321c924c8e97f048e9a5a8733f9dd859bea99
Ozan UYKUN <znframework@gmail.com>
2018-01-21 16:20:15 +0300

    Updated Package

--- Projects/Frontend/Controllers/home.php
+++ Projects/Frontend/Controllers/home.php
@@ -4,8 +4,6 @@ class Home extends Controller
 {
     public function main(String $params = NULL)
     {  
-        output(\CDN::api('?1140'));
-
         View::pageTitle('ZN')
             ->pageSubtitle('"Simplicity is the ultimate sophistication" - Da Vinci');
     } 
