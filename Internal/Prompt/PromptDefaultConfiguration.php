e66b3352c56319be0991b11efb54c66f70ce6412
Ozan UYKUN <znframework@gmail.com>
2018-01-21 12:05:28 +0300

    Updated Prompt Library

---
+++ Internal/Prompt/PromptDefaultConfiguration.php
@@ -0,0 +1,29 @@
+<?php namespace ZN\Prompt;
+/**
+ * ZN PHP Web Framework
+ * 
+ * "Simplicity is the ultimate sophistication." ~ Da Vinci
+ * 
+ * @package ZN
+ * @license MIT [http://opensource.org/licenses/MIT]
+ * @author  Ozan UYKUN [ozan@znframework.com]
+ */
+
+/**
+ * Default Prompt Configuration
+ * 
+ * Enabled when the configuration file can not be accessed.
+ */
+class PromptDefaultConfiguration
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Crontab
+    |--------------------------------------------------------------------------
+    |
+    | The current PHP path. Especially necessary for crontab.
+    |
+    */
+
+    public $path = '/usr/bin/php';
+}
