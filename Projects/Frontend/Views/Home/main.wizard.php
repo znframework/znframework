439da37fe23247c4e4972d8e674e7a044f3aefdd
Ozan UYKUN <znframework@gmail.com>
2018-04-28 22:25:45 +0300

    Updated style.

--- Projects/Frontend/Views/Home/main.wizard.php
+++ Projects/Frontend/Views/Home/main.wizard.php
@@ -1,6 +1,6 @@
 <div id="container">
     <div id="content">
-        <div id="title">{{$pageTitle}} {{ZN_VERSION}} is dedicated to <span class="blue-color">{{ZN_DEDICATE}}</span></div>
-        <div id="sub-title">{{$pageSubtitle}}</div>
+        <div id="title">{{$pageTitle}} <span class="blue-color">{{ZN_VERSION}}</span> is dedicated to <span class="orange-color">{{ZN_DEDICATE}}</span></div>
+        <div id="sub-title" class="blue-color">{{$pageSubtitle}}</div>
     </div>
 </div>
