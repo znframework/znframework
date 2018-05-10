7b5b64ea27df1eb1b541cb60bbe798df81dbfdd8
Ozan UYKUN <znframework@gmail.com>
2018-05-10 17:16:46 +0300

    Fixed External  templates.

--- External/Resources/Templates/UserEmail/Activation.php
+++ External/Resources/Templates/UserEmail/Activation.php
@@ -2,13 +2,13 @@
 
 <table class="table table-bordered">
     <tr>
-        <th><?php echo ZN\Lang::select('IndividualStructures', 'user:activationProcess')?></th>
+        <th><?php echo ZN\Lang::select('Authentication', 'activationProcess')?></th>
     </tr>
 
     <tr>
         <td>
-            <a href="<?php echo $url.'user/'.$user.'/pass/'.$pass; ?>"> <?php echo ZN\Lang::select('IndividualStructures', 'user:activation')?> </a>
+            <a href="<?php echo $url.'user/'.$user.'/pass/'.$pass; ?>"> <?php echo ZN\Lang::select('Authentication', 'activation')?> </a>
         </td>
     </tr>
-</pre>
+</table>
 
--- External/Resources/Templates/UserEmail/ForgotPassword.php
+++ External/Resources/Templates/UserEmail/ForgotPassword.php
@@ -2,20 +2,20 @@
 
 <table class="table table-bordered">
     <tr>
-        <th colspan="2"><?php echo ZN\Lang::select('IndividualStructures', 'user:verificationEmail')?></th>
+        <th colspan="2"><?php echo ZN\Lang::select('Authentication', 'verificationEmail')?></th>
     </tr>
     <tr>
-        <td><?php echo ZN\Lang::select('IndividualStructures', 'user:username')?></td>
+        <td><?php echo ZN\Lang::select('Authentication', 'username')?></td>
         <td><?php echo $usernameColumn; ?></td>
     </tr>
     <tr>
-        <td><?php echo ZN\Lang::select('IndividualStructures', 'user:password')?></td>
+        <td><?php echo ZN\Lang::select('Authentication', 'password')?></td>
         <td><?php echo $newPassword; ?></td>
     </tr>
 
     <tr>
         <td colspan="2">
-            <a href="<?php echo $returnLinkPath; ?>"><?php echo ZN\Lang::select('IndividualStructures', 'user:learnNewPassword'); ?></a>
+            <a href="<?php echo $returnLinkPath; ?>"><?php echo ZN\Lang::select('Authentication', 'learnNewPassword'); ?></a>
         </td>
     </tr>
 </pre>
