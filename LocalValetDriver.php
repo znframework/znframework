c5b0e93ab3546a7952ba00b82041315f4ab72af0
Mert Özgür Akdeniz <mertakdeniz@std.iyte.edu.tr>
2018-06-02 20:29:52 +0300

    Create LocalValetDriver.php
    Adds support for Laravel Valet environment.

---
+++ LocalValetDriver.php
@@ -0,0 +1,53 @@
+<?php
+
+class LocalValetDriver extends BasicValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param string $sitePath
+     * @param string $siteName
+     * @param string $uri
+     *
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return file_exists($sitePath.'/zeroneed.php');
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param string $sitePath
+     * @param string $siteName
+     * @param string $uri
+     *
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        if ($this->isActualFile($staticFilePath = $sitePath . $uri)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param string $sitePath
+     * @param string $siteName
+     * @param string $uri
+     *
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        $_SERVER['SCRIPT_FILENAME'] = $sitePath.'/zeroneed.php';
+        $_SERVER['SCRIPT_NAME'] = '/zeroneed.php';
+
+        return $sitePath.'/zeroneed.php';
+    }
+}
