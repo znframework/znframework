e866eb5b3bd7b24c5409513d4d406db9c4efcfdb
Ozan Uykun <znframework@gmail.com>
2018-06-27 19:16:44 +0300

    Updated LocalValetDriver.

--- LocalValetDriver.php
+++ LocalValetDriver.php
@@ -13,12 +13,10 @@ class LocalValetDriver extends BasicValetDriver
      * Determine if the driver serves the request.
      *
      * @param string $sitePath
-     * @param string $siteName
-     * @param string $uri
      *
      * @return bool
      */
-    public function serves($sitePath, $siteName, $uri)
+    public function serves($sitePath)
     {
         return file_exists($this->sitePath($sitePath));
     }
@@ -27,12 +25,11 @@ class LocalValetDriver extends BasicValetDriver
      * Determine if the incoming request is for a static file.
      *
      * @param string $sitePath
-     * @param string $siteName
      * @param string $uri
      *
      * @return string|false
      */
-    public function isStaticFile($sitePath, $siteName, $uri)
+    public function isStaticFile($sitePath, $uri)
     {
         if( $this->isActualFile($staticFilePath = $sitePath . $uri) ) 
         {
@@ -46,12 +43,10 @@ class LocalValetDriver extends BasicValetDriver
      * Get the fully resolved path to the application's front controller.
      *
      * @param string $sitePath
-     * @param string $siteName
-     * @param string $uri
      *
      * @return string
      */
-    public function frontControllerPath($sitePath, $siteName, $uri)
+    public function frontControllerPath($sitePath)
     {
         $_SERVER['SCRIPT_FILENAME'] = $this->sitePath($sitePath);
         $_SERVER['SCRIPT_NAME']     = $this->directoryIndex();
