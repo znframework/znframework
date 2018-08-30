fe36e087b65f8a3166bd375cffc0cbf9933d522a
Ozan Uykun <znframework@gmail.com>
2018-08-30 12:44:49 +0300

    Modified opening page.

--- Projects/Frontend/Views/Home/main.wizard.php
+++ Projects/Frontend/Views/Home/main.wizard.php
@@ -1,17 +1,7 @@
 <div id="container">
     <div id="content">
-        <div id="title">ZN <span class="blue-color">{{ZN_VERSION}}</span> is dedicated to <span class="blue-color">{{ZN_DEDICATE}}</span></div>
-        <div id="sup-title-2" class="grey-color">
-            n
-        </div>
-        <div id="sub-title" class="grey-color">
-            Arf(q) = <span class="sum">&sum;</span> q(a<sub>i</sub>)q(b<sub>i</sub>) &isin; &Zeta;<sub>2</sub>
-        </div>
-        <div id="sub-title-2" class="grey-color">
-            i=1
-        </div>
-        <div id="sub-title-3" class="grey-color">
-            a<sub>i</sub>, b<sub>i</sub> i = 1, 2, 3, ..., n
-        </div>
+        <div id="title">ZN <span class="blue-color">5.9</span> is dedicated to <span class="blue-color">{{ZN_DEDICATE}}</span></div>
+        <div id="sub-title" class="grey-color">There is only one happiness in this life, to love and be loved.</div>
+        <div id="spacex-logo"></div>
     </div>
 </div>
