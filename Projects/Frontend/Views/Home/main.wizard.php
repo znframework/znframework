af32b052981ab88f567fd5a3938ba70a24d089b3
Ozan Uykun <znframework@gmail.com>
2018-09-05 21:13:46 +0300

    Updated opening page.

--- Projects/Frontend/Views/Home/main.wizard.php
+++ Projects/Frontend/Views/Home/main.wizard.php
@@ -1,7 +1,6 @@
 <div id="container">
     <div id="content">
-        <div id="title">ZN <span class="blue-color">5.9</span> is dedicated to <span class="blue-color">{{ZN_DEDICATE}}</span></div>
-        <div id="sub-title" class="grey-color">There is only one happiness in this life, to love and be loved.</div>
-        <div id="spacex-logo"></div>
+        <div id="title">ZN <span class="blue-color">EON</span> is dedicated to <span class="blue-color">{{ZN_DEDICATE}}</span></div>
+        <div id="sub-title" class="grey-color">Maybe the sunset is an incredible opportunity to find new horizons.</div>
     </div>
 </div>
