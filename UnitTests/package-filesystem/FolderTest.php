ee07d0e908126fb8be4368516dce9fefc85f4816
ozanuykun <ozanbote@gmail.com>
2021-01-28 20:02:37 +0300

    Added Fileysystem\FolderTest class.

---
+++ UnitTests/package-filesystem/FolderTest.php
@@ -0,0 +1,89 @@
+<?php namespace ZN\Filesystem;
+
+use Folder;
+
+class FolderTest extends \PHPUnit\Framework\TestCase
+{
+    const directory = 'UnitTests/package-filesystem/resources/';
+
+    public function testCreate()
+    {
+        Folder::create($directory = self::directory . 'example');
+
+        $this->assertDirectoryExists($directory);
+    }
+
+    public function testRename()
+    {
+        $return = Folder::rename(self::directory . 'example', self::directory . 'test');
+
+        $this->assertIsBool($return);
+    }
+
+    public function testCopy()
+    {
+        $return = Folder::copy(self::directory . 'test', self::directory . 'default');
+
+        $this->assertIsBool($return);
+    }
+
+    public function testBasePath()
+    {
+        $this->assertSame(getcwd(), Folder::basePath());
+    }
+
+    public function testGetFileInfo()
+    {
+        $this->assertIsArray((Folder::fileInfo(self::directory)));
+    }
+
+    public function testDisk()
+    {
+        $this->assertIsFloat(Folder::disk(self::directory));
+        $this->assertIsFloat(Folder::disk(self::directory, 'total'));
+    }
+
+    public function testFiles()
+    {
+        $files     = Folder::files(self::directory);
+        $someFiles = Folder::files(self::directory, ['txt']);
+
+        $this->assertTrue(count($files) > count($someFiles));
+    }
+
+    public function testAllFiles()
+    {
+        $this->assertIsArray(Folder::allFiles(self::directory));
+    }
+
+    public function testPermission()
+    {
+        Folder::permission(self::directory . 'example', 0644);
+
+        $this->assertIsBool(Folder::info(self::directory . 'example')->writable);
+    }
+
+    public function testDelete()
+    {
+        if( is_dir(self::directory . 'example') )
+        {
+            $this->assertTrue(Folder::delete(self::directory . 'example'));
+        }
+        else
+        {
+            $this->assertFalse(false);
+        }
+    }
+
+    public function testExists()
+    {
+        if( Folder::exists(self::directory . 'example') )
+        {
+            $this->assertTrue(Folder::delete(self::directory . 'example'));
+        }
+        else
+        {
+            $this->assertFalse(false);
+        }
+    }
+}
