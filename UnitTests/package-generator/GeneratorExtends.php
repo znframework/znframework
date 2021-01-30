7884ebed111165be136406126db78d2aeb4a4fe0
ozanuykun <ozanbote@gmail.com>
2021-01-31 02:46:00 +0300

    Added Generator\GeneratorExtends class.

---
+++ UnitTests/package-generator/GeneratorExtends.php
@@ -0,0 +1,18 @@
+<?php namespace ZN\Generator\Test;
+
+use Config;
+
+class GeneratorExtends extends \PHPUnit\Framework\TestCase
+{
+    public function __construct()
+    {
+        parent::__construct();
+
+        Config::database('database', 
+        [
+            'driver'   => 'sqlite',
+            'database' => 'UnitTests/package-database/testdb',
+            'password' => '1234'
+        ]);
+    }
+}
