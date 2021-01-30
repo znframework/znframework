fbbfb8763b21ed7e8ff0b77c25122c5be103499f
ozanuykun <ozanbote@gmail.com>
2021-01-31 02:49:46 +0300

    Added Generator\LibraryTest class.

---
+++ UnitTests/package-generator/LibraryTest.php
@@ -0,0 +1,23 @@
+<?php namespace ZN\Generator;
+
+use Generate;
+
+class LibraryTest extends Test\GeneratorExtends
+{
+    public function testLibrary()
+    {
+        Generate::library('Example', 
+        [
+            'implements' => 'TestLibInterface',
+            'extends'    => 'Controller',
+            'namespace'  => 'TestExample',
+            'use'        => ['Folder', 'File' => 'F'],
+            'functions'  => ['main', 'test' => ['a', 'int b' => '10', 'c', 'string ...args']],
+            'constants'  => ['TEST' => '"Test"', 'NUM' => '10'],
+            'traits'     => ['abc', 'xxx'],
+            'vars'       => ['protected:test' => '10', 'examples', 'test2' => '"Test"' ]
+        ]);
+
+        $this->assertFileExists(LIBRARIES_DIR . 'Example.php');
+    }
+}
