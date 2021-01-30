8e6da062f922d1b5a4aff970fecde7ce3c10c014
ozanuykun <ozanbote@gmail.com>
2021-01-30 20:05:00 +0300

    Added Language\LangTest class.

---
+++ UnitTests/package-language/LangTest.php
@@ -0,0 +1,36 @@
+<?php namespace ZN\Language;
+
+use Lang;
+use Config;
+
+class LangTest extends \PHPUnit\Framework\TestCase
+{
+    public function testSet()
+    {
+        Lang::set('tr');
+
+        $this->assertSame('tr', Lang::get());
+    }
+
+    public function testGet()
+    {
+        Lang::set('en');
+
+        $this->assertSame('en', Lang::get());
+    }
+
+    public function testCurrent()
+    {
+        $this->assertSame(false, Lang::current());
+
+        Config::services('uri', ['lang' => true]);
+
+        $this->assertSame('en', Lang::current());
+    }
+
+    public function testSelect()
+    {
+        $this->assertSame('Operation failed!', Lang::select('Error', 'error'));
+        $this->assertSame('`TEST` Invalid Command!', Lang::select('Commands', 'invalidCommand', ['%' => 'TEST']));
+    }
+}
