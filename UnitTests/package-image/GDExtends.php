d6330f3a2b27a6d087a41878c76c1ff4a86e3d96
ozanuykun <ozanbote@gmail.com>
2021-01-31 02:14:58 +0300

    Added Image\GDExtends class.

---
+++ UnitTests/package-image/GDExtends.php
@@ -0,0 +1,7 @@
+<?php namespace ZN\Image\Test;
+
+class GDExtends extends \PHPUnit\Framework\TestCase
+{
+    const dir = 'UnitTests/package-image/resources/';
+    const img = self::dir . 'image.jpg';
+}
