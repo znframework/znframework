c359c6b37b16be486140f3d104b63e784dcff86e
ozanuykun <ozanbote@gmail.com>
2021-01-30 12:48:29 +0300

    Added Hypertext\StyleTest class.

---
+++ UnitTests/package-hypertext/StyleTest.php
@@ -0,0 +1,43 @@
+<?php namespace ZN\Hypertext;
+
+use Style;
+use Buffer;
+
+class StyleTest extends \PHPUnit\Framework\TestCase
+{
+    public function testOpen()
+    {
+        $this->assertStringContainsString
+        (
+            '<style type="text/css">', 
+            Style::open()
+        );
+    }
+    
+    public function tesClose()
+    {
+        $this->assertStringContainsString
+        (
+            '</style', 
+            Style::close()
+        );
+    }
+
+    public function testType()
+    {
+        $this->assertStringContainsString
+        (
+            '<style type="application/css">', 
+            Style::type('application/css')->open()
+        );
+    }
+
+    public function testLibrary()
+    {
+        $this->assertStringContainsString
+        (
+            'bootstrap.min.css', 
+            Buffer::callback(function(){ Style::library('bootstrap')->open(); })
+        );
+    }
+}
