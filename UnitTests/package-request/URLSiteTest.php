c599c8419d7b9747908ac4712f6a9f84ade90b67
ozanuykun <ozanbote@gmail.com>
2021-01-31 11:58:02 +0300

    Extended Request\URLSiteTest class.

--- UnitTests/package-request/URLSiteTest.php
+++ UnitTests/package-request/URLSiteTest.php
@@ -1,6 +1,8 @@
 <?php namespace ZN\Request;
 
 use URL;
+use Lang;
+use Config;
 
 class URLSiteTest extends \PHPUnit\Framework\TestCase
 {
@@ -13,5 +15,14 @@ class URLSiteTest extends \PHPUnit\Framework\TestCase
     public function testUrlSiteFirstParameter()
     {
         $this->assertStringContainsString('/Home/test', URL::site('Home/test'));
+    }
+
+    public function testUrlSiteLangFix()
+    {
+        Config::services('uri', ['lang' => true]); Lang::set('en');
+
+        $this->assertStringContainsString('/en/resources/style.css', URL::site('resources/style.css'));
+
+        Config::services('uri', ['lang' => false]);
     }
 }
