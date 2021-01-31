5c98b40d4b80646272f4082213219ed2d49e3404
ozanuykun <ozanbote@gmail.com>
2021-01-31 17:53:09 +0300

    Extended Request\URIManipulationTest class.

--- UnitTests/package-request/URIManipulationTest.php
+++ UnitTests/package-request/URIManipulationTest.php
@@ -17,4 +17,25 @@ class URIManipulationTest extends \PHPUnit\Framework\TestCase
 
         $this->assertEquals('contact/us/sendForm/example', URI::manipulation(['contact', 'sendForm' => 'example']));
     }
+
+    public function testUriManiputlationBothFix()
+    {
+        $_SERVER['PATH_INFO'] = 'contact/us/sendForm';
+
+        $this->assertEquals('/contact/us/sendForm/example/', URI::manipulation(['contact', 'sendForm' => 'example'], 'both'));
+    }
+
+    public function testUriManiputlationLeftFix()
+    {
+        $_SERVER['PATH_INFO'] = 'contact/us/sendForm';
+
+        $this->assertEquals('/contact/us/sendForm/example', URI::manipulation(['contact', 'sendForm' => 'example'], 'left'));
+    }
+
+    public function testUriManiputlationRightFix()
+    {
+        $_SERVER['PATH_INFO'] = 'contact/us/sendForm';
+
+        $this->assertEquals('contact/us/sendForm/example/', URI::manipulation(['contact', 'sendForm' => 'example'], 'right'));
+    }
 }
