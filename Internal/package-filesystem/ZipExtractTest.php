5d681f83d4af59fe4e9de074a280d2ebc2d04523
ozanuykun <ozanbote@gmail.com>
2021-02-01 20:36:40 +0300

    Extended Filesystem\ZipExtractTest class.

--- Internal/package-filesystem/ZipExtractTest.php
+++ Internal/package-filesystem/ZipExtractTest.php
@@ -6,11 +6,19 @@ class ZipExtractTest extends FilesystemExtends
 {
     public function testZipExtract()
     {
-        File::create($file1 = self::directory . 'zip-file-1.txt');
-        File::create($file2 = self::directory . 'zip-file-2.txt');
+        File::create($file1 = self::directory . '1.txt');
+        File::create($file2 = self::directory . '2.txt');
+
         File::createZip($zipFile = self::directory . 'example.zip', [$file1, $file2]);
+        
         File::zipExtract($zipFile, $directory = self::directory . 'extract/');
 
         $this->assertDirectoryExists($directory);
+
+        File::delete($file1);
+        File::delete($file2);
+        File::delete($zipFile);
+        
+        Folder::delete($directory);
     }
 }
