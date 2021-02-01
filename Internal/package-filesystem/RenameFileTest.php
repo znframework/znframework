27a48726e9f1c96b48f3937f3dead9013eccd948
ozanuykun <ozanbote@gmail.com>
2021-02-01 20:15:21 +0300

    Extended Filesystem\RenameFileTest class.

--- Internal/package-filesystem/RenameFileTest.php
+++ Internal/package-filesystem/RenameFileTest.php
@@ -6,8 +6,12 @@ class RenameFileTest extends FilesystemExtends
 {
     public function testRename()
     {
+        File::create(self::file);
+
         File::rename(self::file, $file = self::directory . 'rename-file.txt');
 
         $this->assertFileExists($file);
+
+        File::delete($file);
     }
 }
