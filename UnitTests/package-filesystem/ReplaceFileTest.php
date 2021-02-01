4d7389821525333698cd68ffc4de0cfb864adc6b
ozanuykun <ozanbote@gmail.com>
2021-02-01 14:37:03 +0300

    Added Filesystem\ReplaceFileTest class.

---
+++ UnitTests/package-filesystem/ReplaceFileTest.php
@@ -0,0 +1,15 @@
+<?php namespace ZN\Filesystem;
+
+use File;
+
+class ReplaceFileTest extends FilesystemExtends
+{
+    public function testReplace()
+    {
+        File::write(self::file, 'test');
+
+        File::replace(self::file, 'test', 'example');
+
+        $this->assertSame('example', File::read(self::file));
+    }
+}
