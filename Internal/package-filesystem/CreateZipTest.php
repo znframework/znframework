a7745bd7094d7056b7b4e27a2ebc0bbeb5026bcc
ozanuykun <ozanbote@gmail.com>
2021-02-01 19:54:23 +0300

    Extended Filesystem\CreateZipTest class.

--- Internal/package-filesystem/CreateZipTest.php
+++ Internal/package-filesystem/CreateZipTest.php
@@ -6,11 +6,15 @@ class CreateZipTest extends FilesystemExtends
 {
     public function testCreateZip()
     {
-        File::create(self::directory . 'zip-file-1.txt');
-        File::create(self::directory . 'zip-file-2.txt');
-
-        File::createZip($zipFile = self::directory . 'example.zip', [self::directory . 'zip-file-1.txt', self::directory . 'zip-file-2.txt']);
+        File::create($file1 = self::directory . '1.txt');
+        File::create($file2 = self::directory . '2.txt');
+        
+        File::createZip($zipFile = self::directory . 'example.zip', [$file1, $file2]);
 
         $this->assertFileExists($zipFile);
+
+        File::delete($file1);
+        File::delete($file2);
+        File::delete($zipFile);
     }
 }
