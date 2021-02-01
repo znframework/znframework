43a990087c6c6af5b83e387ab410e57b3df98ab0
ozanuykun <ozanbote@gmail.com>
2021-02-01 14:37:23 +0300

    Added Filesystem\TruncateFileTest class.

---
+++ UnitTests/package-filesystem/TruncateFileTest.php
@@ -0,0 +1,15 @@
+<?php namespace ZN\Filesystem;
+
+use File;
+
+class TruncateFileTest extends FilesystemExtends
+{
+    public function testTruncate()
+    {
+        File::write(self::file, 'example');
+        
+        File::truncate(self::file, 2);
+
+        $this->assertSame('ex', File::read(self::file));
+    }
+}
