19725e3653dab16fede2dcbe9959dfafe8c00dac
ozanuykun <ozanbote@gmail.com>
2021-02-01 20:14:03 +0300

    Extended Filesystem\ReadZipTest class.

--- Internal/package-filesystem/ReadZipTest.php
+++ Internal/package-filesystem/ReadZipTest.php
@@ -6,12 +6,17 @@ class ReadZipTest extends FilesystemExtends
 {
     public function testReadZip()
     {
-        File::create($file1 = self::directory . 'zip-file-1.txt');
-        File::create($file2 = self::directory . 'zip-file-2.txt');
+        File::create($file1 = self::directory . '1.txt');
+        File::create($file2 = self::directory . '2.txt');
+        
         File::createZip($zipFile = self::directory . 'example.zip', [$file1, $file2]);
 
         $zipFiles = File::readZip($zipFile);
 
         $this->assertEquals([$file1, $file2], $zipFiles);
+
+        File::delete($file1);
+        File::delete($file2);
+        File::delete($zipFile);
     }
 }
