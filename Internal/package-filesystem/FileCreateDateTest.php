1267687f0aecbb75dd39a94e640b95f1e2e66c83
ozanuykun <ozanbote@gmail.com>
2021-02-01 20:01:07 +0300

    Extended Filesystem\FileCreateDateTest class.

--- Internal/package-filesystem/FileCreateDateTest.php
+++ Internal/package-filesystem/FileCreateDateTest.php
@@ -7,6 +7,10 @@ class FileCreateDateTest extends FilesystemExtends
 {
     public function testCreateDate()
     {
+        File::create(self::file);
+
         $this->assertTrue(Date::check(File::createDate(self::file), 'Y'));
+
+        File::delete(self::file);
     }
 }
