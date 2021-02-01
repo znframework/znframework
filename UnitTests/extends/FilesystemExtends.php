d9bdf2e670eb052fbe5b19ce8443f12d0949eee9
ozanuykun <ozanbote@gmail.com>
2021-02-01 13:55:54 +0300

    Added Filesystem\FilesystemExtends class.

---
+++ UnitTests/extends/FilesystemExtends.php
@@ -0,0 +1,7 @@
+<?php namespace ZN\Filesystem;
+
+class FilesystemExtends extends \PHPUnit\Framework\TestCase
+{
+    const directory = 'UnitTests/package-filesystem/resources/';
+    const file      = self::directory . 'write-file.txt';
+}
