0bdfe7d9e4f7b208727463145c826f99dbd25231
ozanuykun <ozanbote@gmail.com>
2021-02-01 14:38:14 +0300

    Added Filesystem\WriteFileTest class.

---
+++ UnitTests/package-filesystem/WriteFileTest.php
@@ -0,0 +1,13 @@
+<?php namespace ZN\Filesystem;
+
+use File;
+
+class WriteFileTest extends FilesystemExtends
+{
+    public function testWrite()
+    {
+        File::write(self::file, 'test');
+
+        $this->assertFileExists(self::file);
+    }
+}
