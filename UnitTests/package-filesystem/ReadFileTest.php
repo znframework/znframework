1be1417a41e600451da155bd8bc6ba2471b40f90
ozanuykun <ozanbote@gmail.com>
2021-02-01 14:36:00 +0300

    Added Filesystem\ReadFileTest class.

---
+++ UnitTests/package-filesystem/ReadFileTest.php
@@ -0,0 +1,13 @@
+<?php namespace ZN\Filesystem;
+
+use File;
+
+class ReadFileTest extends FilesystemExtends
+{
+    public function testRead()
+    {
+        File::write(self::file, 'test');
+        
+        $this->assertSame('test', File::read(self::file));
+    }
+}
