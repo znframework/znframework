c1a53fe4f5908dc59f98e09d80b444a977ab55e3
ozanuykun <ozanbote@gmail.com>
2021-02-01 20:09:37 +0300

    Extended Filesystem\FolderDiskTest class.

--- Internal/package-filesystem/FolderDiskTest.php
+++ Internal/package-filesystem/FolderDiskTest.php
@@ -6,7 +6,11 @@ class FolderDiskTest extends FilesystemExtends
 {
     public function testDisk()
     {
-        $this->assertIsFloat(Folder::disk(self::directory));
-        $this->assertIsFloat(Folder::disk(self::directory, 'total'));
+        Folder::create(self::dir);
+
+        $this->assertIsFloat(Folder::disk(self::dir));
+        $this->assertIsFloat(Folder::disk(self::dir, 'total'));
+
+        Folder::delete(self::dir);
     }
 }
