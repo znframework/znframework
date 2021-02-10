eebd11aae71277804f994b15630b8cef3da8912e
ozanuykun <ozanbote@gmail.com>
2021-02-10 14:08:35 +0300

    Released 6.0.6

--- Projects/Frontend/Config/ViewObjects.php
+++ Projects/Frontend/Config/ViewObjects.php
@@ -110,55 +110,6 @@
 
     /*
     |--------------------------------------------------------------------------
-    | Datagrid
-    |--------------------------------------------------------------------------
-    |
-    | Includes default settings for the datagrids.
-    |
-    */
-
-    'dbgrid' =>
-    [
-        'styleElement' =>
-        [
-            '#DBGRID_TABLE tr:nth-child(even)' => ['background' => '#E6F9FF'],
-            '#DBGRID_TABLE tr:nth-child(odd)'  => ['background' => '#FFF']
-        ],
-        'attributes'   =>
-        [
-            'table'         => ['width' => '100%', 'cellspacing' => 0, 'cellpadding' => 10, 'style' => 'margin-top:15px; margin-bottom:15px; border:solid 1px #ddd; font-family:Arial; color:#888; font-size:14px;'],
-            'editTables'    => ['style' => 'font-family:Arial; color:#888; font-size:14px;'],
-            'columns'       => ['height' => 75, 'style' => 'text-decoration:none; color:#0085B2'],
-            'search'        => ['style' => 'height:34px; color:#0085B2; border:solid 1px #0085B2; text-indent:10px'],
-            'add'           => ['style' => 'height:34px; color:#0085B2; background:none; border:solid 1px #0085B2; cursor:pointer'],
-            'deleteSelected'=> ['style' => 'height:34px; color:#0085B2; background:none; border:solid 1px #0085B2; cursor:pointer'],
-            'deleteAll'     => ['style' => 'height:34px; color:#0085B2; background:none; border:solid 1px #0085B2; cursor:pointer'],
-            'save'          => ['style' => 'height:34px; color:#0085B2; background:none; border:solid 1px #0085B2; cursor:pointer'],
-            'update'        => ['style' => 'height:34px; color:#0085B2; background:none; border:solid 1px #0085B2; cursor:pointer'],
-            'delete'        => ['style' => 'height:34px; color:#0085B2; background:none; border:solid 1px #0085B2; cursor:pointer'],
-            'edit'          => ['style' => 'height:34px; color:#0085B2; background:none; border:solid 1px #0085B2; cursor:pointer'],
-            'listTables'    => [],
-            'inputs'        =>
-            [
-                'text'      => ['style' => 'height:34px; color:#0085B2; border:solid 1px #0085B2; text-indent:10px'],
-                'textarea'  => ['style' => 'height:120px; width:290px; color:#0085B2; border:solid 1px #0085B2; text-indent:10px'],
-                'radio'     => [],
-                'checkbox'  => [],
-                'select'    => []
-            ]
-        ],
-        'pagination' =>
-        [
-            'style' =>
-            [
-                'links'   => 'color:#0085B2;width:30px; height:30px;text-align:center;padding-top:4px;display:inline-block;background:white;border:solid 1px #ddd;border-radius: 4px;-webkit-border-radius: 4px;-moz-border-radius: 4px;text-decoration:none;',
-                'current' => 'font-weight:bold;'
-            ]
-        ]
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
     | ML Grid
     |--------------------------------------------------------------------------
     |
